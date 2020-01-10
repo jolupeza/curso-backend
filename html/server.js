89d0994fff6d8c04f93b5e8f10339add9fd49c5d
Jose Perez <jose.perez@altimea.com>
2020-01-10 12:25:39 -0500

    Initial commit

---
+++ html/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use('/public', express.static('assets'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.listen(3000);
